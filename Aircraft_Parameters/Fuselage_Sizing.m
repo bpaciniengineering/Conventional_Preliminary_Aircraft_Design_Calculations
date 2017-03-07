41a8d4230eb6d1a0453527d0d1139f447852748f
mtravnik <madeline.travnik@gmail.com>
2017-03-07 11:14:50 -0500

    Update Fuselage_Sizing.m

--- Aircraft_Parameters/Fuselage_Sizing.m
+++ Aircraft_Parameters/Fuselage_Sizing.m
@@ -5,9 +5,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function [L_fuse, D_max_fuse] = fuselage_sizing(a,C,Wo,fineness)
+function [L_fus, D_max_fus] = fuselage_sizing(a,C,Wo,fineness)
 
 L_fus = a*Wo^C % ft
 
-D_max_fuse = L_fus/fineness % ft
+D_max_fus = L_fus/fineness % ft
 
