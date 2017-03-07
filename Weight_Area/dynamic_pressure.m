0ff9b96550ebe0444d01788bdcbe809cb854b2ad
Nathan Wei <nwei@princeton.edu>
2017-03-07 11:28:20 -0500

    Fixed function name typo, oops

---
+++ Weight_Area/dynamic_pressure.m
@@ -0,0 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Bernardo Pacini                                                       %%
+%% MAE 332 - Aircraft Design                                             %%
+%% Preliminary Design Calculations                                       %%
+%% Feb. 20, 2017 Mon                                                     %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [ output_args ] = dynamic_pressure( input_args )
+
+end
+
