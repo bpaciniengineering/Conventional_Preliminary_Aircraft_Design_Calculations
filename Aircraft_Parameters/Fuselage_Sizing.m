09baf368f264ba811d5efb8fee107e67b87b0060
mtravnik <madeline.travnik@gmail.com>
2017-03-08 14:45:03 -0500

    Update Fuselage_Sizing.m

--- Aircraft_Parameters/Fuselage_Sizing.m
+++ Aircraft_Parameters/Fuselage_Sizing.m
@@ -4,10 +4,30 @@
 % 3 March 2017
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Requires a, C, Wo, fineness ratio, length of cockpit, and upsweep angle.
+% Returns fuselage length, maximum fuselage diameter, moment arm of horizontal tail,
+% and volume of fuselage (using top and side area estimations).
 
-function [L_fus, D_max_fus, L_HT] = fuselage_sizing(a,C,Wo,fineness);
+function [L_fus, D_max_fus, L_HT, V_fus] = ...
+                fuselage_sizing(a,C,Wo,fineness,L_cockpit,upsweep);
 
 L_fus = a*Wo^C; % ft
 D_max_fus = L_fus/fineness; % ft
-L_HT =   0.45 * L_fus; % ft, H tail moment arm : aft engine per Raymer 160: 0.45 * L_fus
+L_HT =   0.45 * L_fus; % ft, H tail moment arm : aft engine per Raymer 160
 
+% Top View Calculation
+A_top = D_max_fus*L_fus-2*(.5 * (.4 * D_max_fus)*L_cockpit); 
+                     % ft, assuming that the diameter of the nose is 1/5 
+                     % diameter of fuselage from top view
+
+% Side View Calculation
+L_fus_angled = D_max_fus/ tan(upsweep); % ft, horizontal distance from rear
+                                       % where fuselage angles upward
+A_side = D_max_fus*L_fus-(.5*D_max_fus*L_fus_angled)-...
+    2*(.5*(.6*D_max_fus)*L_cockpit);
+                     % ft, assuming that the diameter of the nose is 2/5 
+                     % diameter of fuselage from side view
+
+                     
+% Volume Calculation
+V_fus = 3.4 * A_top * A_side / (4*L_fus);
