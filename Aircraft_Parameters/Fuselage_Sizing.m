6c3593d6fb39bcc355d8dbede0ab159cbea143e4
mtravnik <madeline.travnik@gmail.com>
2017-03-07 22:19:45 -0500

    Update Fuselage_Sizing.m

--- Aircraft_Parameters/Fuselage_Sizing.m
+++ Aircraft_Parameters/Fuselage_Sizing.m
@@ -5,9 +5,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-function [L_fus, D_max_fus] = fuselage_sizing(a,C,Wo,fineness);
+function [L_fus, D_max_fus, L_HT] = fuselage_sizing(a,C,Wo,fineness);
 
 L_fus = a*Wo^C; % ft
-
 D_max_fus = L_fus/fineness; % ft
+L_HT =   0.45 * L_fus; % ft, H tail moment arm : aft engine per Raymer 160: 0.45 * L_fus
 
